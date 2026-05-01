f51fe6e779bec2fdb66ff4d3191bb1e9cfb83ae3
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-01 21:14:01 +0300

    Add: sentence-similarity - submission-2

---
+++ Data Structures & Algorithms/sentence-similarity/submission-2.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} sentence1
+     * @param {string[]} sentence2
+     * @param {string[][]} similarPairs
+     * @return {boolean}
+     */
+    areSentencesSimilar(sentence1, sentence2, similarPairs) {
+        const len1 = sentence1.length;
+        const len2 = sentence2.length;
+        // let j = 0;
+        if (len1 !== len2) return false;
+        for (let i = 0; i < len1; i++) {
+            const word1 = sentence1[i];
+            const word2 = sentence2[i];
+            if (word1 === word2) {
+                continue;
+            }
+            // if (similarPairs.length > 0) {
+            //     // loop over first
+            //     // add a hash later
+            //     const x = similarPairs.some((pair) => pair.includes(word1) && pair.includes(word2));
+            //     if (x) {
+            //         continue;
+            //     } else {
+            //         return false;
+            //     }
+            // }
+                const x = similarPairs.some((pair) => pair.includes(word1) && pair.includes(word2));
+                if (x) {
+                    continue;
+                } else {
+                    return false;
+                }
+
+        }
+        return true;
+    }
+}
