320209f6f22f93b7b39abbde6b9be18324cbd0ce
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-06 19:56:36 +0300

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        const set = new Set();
+        let l = 0;
+        for (let r = 0; r < nums.length; r++) {
+            if (r - l > k) {
+                set.delete(nums[l]);
+                l += 1;
+            }
+            if (set.has(nums[r])) {
+                return true;
+            }
+            set.add(nums[r]);
+        }
+        return false;
+    }
+}
