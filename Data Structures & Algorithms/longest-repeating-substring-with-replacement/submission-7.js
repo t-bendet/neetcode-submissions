645f6d14e95b39d7006197dcf68e3dc310207748
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-06 10:04:08 +0300

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        const count = new Map();
+        let res = 0;
+        let l = 0;
+        for (let r = 0; r < s.length; r++) {
+            count.set(s[r], (count.get(s[r]) || 0) + 1);
+            while (r - l + 1 - Math.max(...count.values()) > k) {
+                count.set(s[l], count.get(s[l]) - 1);
+                l += 1;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
