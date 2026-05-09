593f780bcc781e7af31af4550dd4eeccb48fb9ce
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-09 21:25:58 +0300

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+        if (!root) return 0;
+        const leftMax = this.maxDepth(root.left) + 1;
+        const rightMax = this.maxDepth(root.right) + 1;
+        return leftMax > rightMax ? leftMax : rightMax;
+    }
+}
