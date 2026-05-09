f203f01f32ad635d3f5d78fee1087af7dc5d88e0
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-09 19:42:36 +0300

    Add: single-row-keyboard - submission-2

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} keyboard
+     * @param {string} word
+     * @return {number}
+     */
+    calculateTime(keyboard, word) {
+        const map = new Map();
+        let score = 0;
+        for (let i = 0; i < keyboard.length; i++) {
+            map.set(keyboard[i], i);
+        }
+        for (let i = 0; i < word.length; i++) {
+            const prev = i === 0 ? 0:map.get(word[i-1]) 
+            score+= Math.abs((map.get(word[i-1]) || 0) - map.get(word[i]) )
+        }
+
+        return score
+    }
+}
