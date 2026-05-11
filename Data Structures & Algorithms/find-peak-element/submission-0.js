b560e3f00c57d3897fd94064e9cd77cb724813ef
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-11 14:09:14 +0300

    Add: find-peak-element - submission-0

---
+++ Data Structures & Algorithms/find-peak-element/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findPeakElement(nums) {
+                let l = 0,
+            r = nums.length - 1;
+
+        while (l < r) {
+            let m = l + Math.floor((r-l) /2 ) ;
+            if (nums[m] > nums[m + 1]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l;
+    }
+}
