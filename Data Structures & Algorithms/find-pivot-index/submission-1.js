9032383696076f539cd243278acbe6467898acbe
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-23 20:51:20 +0300

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    pivotIndex(nums) {
+        const len = nums.length;
+        const prefix = Array.from({ length: len });
+        for (let i = 0; i < len; i++) {
+            prefix[i] = i === 0 ? nums[i] : prefix[i - 1] + nums[i];
+        }
+        for (let i = 0; i < len; i++) {
+            const pre = i === 0 ? 0 : prefix[i - 1];
+            const post = i === len - 1 ? 0 : prefix[len - 1] - prefix[i];
+            if (pre === post) return i;
+        }
+
+        return -1;
+    }
+}
