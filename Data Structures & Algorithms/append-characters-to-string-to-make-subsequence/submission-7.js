e8014c5cd4e69216abf9347c829863cd3928dfea
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-26 21:06:50 +0300

    Add: append-characters-to-string-to-make-subsequence - submission-7

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-7.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    appendCharacters(s, t) {
+        let si = 0;
+        let ti = 0;
+        while (si < s.length && ti < t.length) {
+            if (s[si] === t[ti]) {
+                si++;
+                ti++;
+                continue;
+            }
+            si++;
+        }
+        return t.length -ti 
+    }
+}
