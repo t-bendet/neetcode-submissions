ad0d19524e8af39e3eead265972035745f93e4e2
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-05-30 21:46:59 +0300

    Add: minimum-size-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} nums
+     * @return {number}
+     */
+    minSubArrayLen(target, nums) {
+        let minSize = Infinity;
+        let runningSum = 0;
+        let l = 0;
+        for (let r = 0; r < nums.length; r++) {
+            const num = nums[r];
+            if (num >= target) return 1;
+            runningSum += num;
+            while (runningSum >= target && r !== l) {
+                minSize = Math.min(minSize, r - l + 1);
+                runningSum-= nums[l]
+                l++;
+            }
+        }
+        return minSize === Infinity ? 0 : minSize;
+    }
+}
