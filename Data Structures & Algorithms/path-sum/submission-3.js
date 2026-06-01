e8874d7e66f4090fb4e6e8a9478c9a17b57c1473
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-06-01 20:35:49 +0300

    Add: path-sum - submission-3

---
+++ Data Structures & Algorithms/path-sum/submission-3.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} targetSum
+     * @return {boolean}
+     */
+    hasPathSum(root, targetSum, runningSum = 0) {
+        console.log({root,left:root?.left || null,right:root?.right|| null})
+        if (!root) return false;
+        runningSum += root.val;
+        if (runningSum === targetSum && !root.left && !root.right) return true;
+        // if (this.hasPathSum(root.left, targetSum, runningSum)) return true;
+        // if (this.hasPathSum(root.right, targetSum, runningSum)) return true;
+        return this.hasPathSum(root.left, targetSum, runningSum) || this.hasPathSum(root.right, targetSum, runningSum);
+    }
+}
