39436c5f8c9d21b8742939da8049f2d006848840
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-06-03 22:09:12 +0300

    Add: binary-tree-inorder-traversal - submission-5

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-5.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    inorderTraversal(root) {
+        const stack = [];
+        const res = [];
+        let curr = root;
+        while (curr || stack.length > 0) {
+            while (curr) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.push(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+}
