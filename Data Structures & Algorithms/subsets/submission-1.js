381beb57eb32afee286714ba838f3f79f2b80820
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-06-04 10:15:50 +0300

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        const res = [];
+        const subset = [];
+        this.dfs(nums, 0, subset, res);
+        return res;
+    }
+    /**
+     * @param {number[]} nums
+     * @param {number} i
+     * @param {number[]} subset
+     * @param {number[][]} res
+     * @return {void}
+     */
+    dfs(nums, i, subset, res) {
+        if (i >= nums.length) {
+            res.push([...subset]);
+            return;
+        }
+
+        // decision to include nums[i]
+        subset.push(nums[i]);
+        this.dfs(nums, i + 1, subset, res);
+        subset.pop();
+
+        // decision to include nums[i]
+        this.dfs(nums, i + 1, subset, res);
+    }
+}
