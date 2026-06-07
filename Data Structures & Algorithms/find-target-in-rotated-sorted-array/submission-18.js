fb33e702cf7dfc79187e6cfa51eb9a0b43c143f6
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-06-07 11:41:05 +0300

    Add: find-target-in-rotated-sorted-array - submission-18

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-18.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+        while (l <= r) {
+            const m = Math.floor((l + r) / 2);
+            const mid = nums[m];
+            const right = nums[r];
+            const left = nums[l]
+            if (target === mid) {
+                return m;
+            }
+
+            if (left <= mid) {
+                if (target > mid || target <left) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                if (target < mid || target >right) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
