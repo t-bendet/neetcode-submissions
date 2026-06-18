66c91c6d0669000b9caad141a248744774c50075
tal bendet <68239430+t-bendet@users.noreply.github.com>
2026-06-18 19:50:03 +0300

    Add: concatenation-of-array - submission-4

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-4.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        let len = nums.length;
+        const ans = Array.from({ length: len * 2 });
+        for (let i = 0; i < len; i++) {
+            ans[i] = ans[i + len] = nums[i];
+        }
+        return ans;
+    }
+}
